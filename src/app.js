97d8b4ee8fd16060d450ead97f2913379ffc0623
Zenzi Ali <zeeathome935@gmail.com>
2020-08-13 20:45:23 -0400

    added key and error messages

--- src/app.js
+++ src/app.js
@@ -28,6 +28,27 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    retun res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
+});
+
+const cards = [{
+  id: 1,
+  title: 'Task One',
+  content: 'This is card one'
+}];
+
+const lists = [{
+  id: 1,
+  header: 'List One',
+  cardIds: [1]
+}];
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
